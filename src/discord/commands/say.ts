8eb91d4b0c3b8b0b55c0d26cf9f114cb28759ccc
SIM7K <62073734+SIM7K@users.noreply.github.com>
2020-09-01 19:56:24 +0200

    lets see

---
+++ src/discord/commands/say.ts
@@ -0,0 +1,19 @@
+import { createDefault } from "../../utils/EmbedUtils"
+import config from "../../../config"
+import { Main, Command } from "../../Main"
+class Say {
+  constructor() {
+    return {
+      name: "say",
+      description: "Say umožňuje oficiálnější sdělování zpráv.",
+      run: async (message, {args}) => {
+        if(!message.member.hasPermission("ADMINISTRATOR")) return await message.reply("nemáš povolení na tento příkaz!")
+        if(!args) return await message.reply("nemám co říct!")
+        if(message.mentions.channels.size == 0) return await message.reply("nezmínil jsi kanál!")
+        message.mentions.channels.first().send(args.join(" "))
+      },
+    } as Command
+  }
+}
+
+export default Say
