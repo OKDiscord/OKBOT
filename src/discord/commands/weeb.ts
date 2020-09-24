2fca7f72c39f2fb6033aa2a32b931aa5dba59a45
VottusCode <vottus@vott.us>
2020-09-24 21:43:14 +0200

    Weeb time: added weeb commands

---
+++ src/discord/commands/weeb.ts
@@ -0,0 +1,48 @@
+import { logger } from "@typegoose/typegoose/lib/logSettings"
+import { create } from "domain"
+import { Command } from "../../types/Command"
+import { createSimple } from "../../utils/EmbedUtils"
+
+class Weeb {
+  constructor() {
+    return {
+      name: "weeb",
+      description: "Command kterej vyšle Japanizing beam!",
+      run: async (message) => {
+        const allowed = ["464857021603250197", "630439552389218313"]
+        if (allowed.includes(message.author.id)) {
+          await message.channel.send(
+            createSimple(
+              "Posílám Japanizing beam",
+              "Připravte se na přeměnu na weeby!"
+            )
+          )
+          for (const member of message.guild.members.cache.array()) {
+            try {
+              if (member.nickname !== "ultra weeb") {
+                await member.setNickname("ultra weeb")
+              }
+            } catch (error) {
+              logger.warn(
+                `Chyba při přejmenování ${member.user.username} - `,
+                error.message
+              )
+            }
+          }
+          return await message.channel.send(
+            createSimple(
+              "Vítejte, weebové!",
+              "Doufáme, že se vám zde bude líbít! @everyone"
+            )
+          )
+        }
+
+        return await message.reply(
+          "nejsi Simír ani Vottus! Nemůžeš poslat japanizing beam!"
+        )
+      },
+    } as Command
+  }
+}
+
+export default Weeb
