635c02e3e863fbf71df93e716474b762ce2680c7
Mia <vottus@vott.us>
2021-01-23 04:02:09 +0100

    refactor: remove all http

--- src/types/context.ts
+++ src/types/context.ts
@@ -1,14 +1,12 @@
 import { Client } from "discord.js"
 import { Mongoose } from "mongoose"
 import { Command } from "./command"
-import { Fasteer } from "@fasteerjs/fasteer"
 
 // Context for all controllers, events and other stuff
 export interface Context {
   client: Client
   db: Mongoose
   commands: Command[]
-  server: Fasteer.Fasteer
 }
 
 export default Context
